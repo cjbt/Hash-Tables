9a2b00de8bc130e3055084fd7e74f604622e93a3
cecil john tantay <cjbtantay@gmail.com>
2019-12-03 19:11:24 -0800

    finish homework

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,21 +1,24 @@
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+
 class HashTable:
     '''
     A hash table that with `capacity` buckets
     that accepts string keys
     '''
+
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-
 
     def _hash(self, key):
         '''
@@ -25,7 +28,6 @@ class HashTable:
         '''
         return hash(key)
 
-
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
@@ -34,7 +36,6 @@ class HashTable:
         '''
         pass
 
-
     def _hash_mod(self, key):
         '''
         Take an arbitrary key and return a valid integer index
@@ -42,50 +43,59 @@ class HashTable:
         '''
         return self._hash(key) % self.capacity
 
-
     def insert(self, key, value):
-        '''
-        Store the value with the given key.
-
-        Hash collisions should be handled with Linked List Chaining.
-
-        Fill this in.
-        '''
-        pass
-
-
+        index = self._hash_mod(key)
+        if self.storage[index] is not None:
+            current = self.storage[index]
+            while current is not None:
+                if current.key == key:
+                    current.value = value
+                    return
+                if current.next is None:
+                    current.next = LinkedPair(key, value)
+                    return
+                current = current.next
+        else:
+            self.storage[index] = LinkedPair(key, value)
 
     def remove(self, key):
-        '''
-        Remove the value stored with the given key.
+        index = self._hash_mod(key)
 
-        Print a warning if the key is not found.
-
-        Fill this in.
-        '''
-        pass
-
+        if self.storage[index] is None:
+            current = self.storage[index]
+            while current is not None:
+                if current.key == key:
+                    current = None
+                    return
+                current = current.next
+            return
+        self.storage[index] = None
 
     def retrieve(self, key):
-        '''
-        Retrieve the value stored with the given key.
+        index = self._hash_mod(key)
 
-        Returns None if the key is not found.
-
-        Fill this in.
-        '''
-        pass
-
+        if self.storage[index] is not None:
+            if self.storage[index].key == key:
+                return self.storage[index].value
+            else:
+                current = self.storage[index]
+                while current is not None:
+                    if current.key == key:
+                        return current.value
+                    current = current.next
+        else:
+            return None
 
     def resize(self):
-        '''
-        Doubles the capacity of the hash table and
-        rehash all key/value pairs.
-
-        Fill this in.
-        '''
-        pass
-
+        self.capacity *= 2
+        old_storage = self.storage
+        new_storage = [None] * self.capacity
+        self.storage = new_storage
+        for item in old_storage:
+            current = item
+            while current is not None:
+                self.insert(current.key, current.value)
+                current = current.next
 
 
 if __name__ == "__main__":
@@ -115,3 +125,5 @@ if __name__ == "__main__":
     print(ht.retrieve("line_3"))
 
     print("")
+
+###
